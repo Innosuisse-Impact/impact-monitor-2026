8b49c2d2389b591d45d4f976926b5a096429669f
aisear <137108084+aisear@users.noreply.github.com>
2026-04-02 15:27:10 +0200

    Change data file format from TSV to CSV

--- src/data.js
+++ src/data.js
@@ -112,7 +112,7 @@ export const df_zufrieden = df_ziel_erfolg
     };
   });
 
-export const df_ergebnisse = await FileAttachment("data/daten_ergebnisse.txt").tsv({ typed: true });
+export const df_ergebnisse = await FileAttachment("data/daten_ergebnisse.csv").csv({ typed: true });
 
 const innovationsart = await FileAttachment("data/daten_innovationsart.csv").csv({ typed: true });
 
